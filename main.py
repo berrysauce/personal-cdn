cd29f87d7ebe8ae1bc67780c494a7a3b8fb7d0df
Paul Haedrich <paul@berrysauce.me>
2021-05-22 19:18:48 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -43,12 +43,6 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 deta = Deta(DETA_TOKEN)
 images = deta.Drive("cdn-images")
 meta = deta.Base("cdn-meta")
-
-
-class Item(BaseModel):
-    image: str
-    file_type: str
-    compress: Optional[bool] = True
     
 
 def generate_html_response():
@@ -80,114 +74,33 @@ def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
 async def root(request: Request):
     return generate_html_response()
 
-@app.get("/image/{imgID}")
-@limiter.limit("1000/minute")
-async def get_image(imgID, request: Request, show_meta: Optional[bool] = False):
-    if show_meta is True:
-        try:
-            return meta.get(imgID)
-        except IOError:
-            raise HTTPException(status_code=500, detail="There is no metadata saved with this image")
-    try:
-        return FileResponse(images.get(imgID))
-    except IOError:
-        raise HTTPException(status_code=404, detail="File not found")
 
-@app.post("/upload")
-@limiter.limit("30/minute")
-async def upload(item: Item, request: Request, username: str = Depends(get_current_username)):
-    try:
-        #tempID = str(random.randint(1000000000,9999999999))
-        
-        #if item.file_type[:1] != ".":
-        #    file_type = "."+item.file_type
-        #else:
-        #    file_type = item.file_type
-
-        # Save Image
-        #with open("/tmp/"+tempID+file_type, "wb") as img:
-        #    img.write(base64.b64decode(item.image))
-            
-        # Compress Image
-        #if item.compress is True and file_type in [".png", ".jpg", ".jpeg"]:
-        #    img = Image.open("/tmp/"+tempID+file_type)
-        #    img.save("/tmp/"+tempID+file_type, optimize=True,quality=80)
-        #    compressed = True
-        #else:
-        #    compressed = False
-        compressed = False
-            
-        # Save Image Metadata    
-        metaResponse = meta.put({"compressed": compressed,
-                        "uploaded_by": username,
-                        "uploaded_on": str(date.today())
-                        })
-        imgID = str(metaResponse["key"])
-        
-        # Upload image and delete local copy
-        images.put(imgID, data=item.image)
-        #images.put(imgID+file_type, path="/tmp/"+tempID+file_type)
-        #os.remove("/tmp/"+tempID+file_type)
-            
-        return {
-            "detail": "Image uploaded",
-            "img_id": imgID,
-            "img_url": "cdn.lab.brry.cc"+"/image/"+imgID,
-            "compressed": compressed
-            }
-    except Exception as exception:
-        raise HTTPException(status_code=500, detail="Error - {0}".format(exception))
-    
-
-@app.get("/form", response_class=HTMLResponse)
+@app.get("/upload", response_class=HTMLResponse)
 def form():
     return """
-    <form action="/upload_form" enctype="multipart/form-data" method="post">
+    <h1>Upload an image<h1>
+    <p>You need a brry Auth account to do this.<p>
+    <form action="/upload" enctype="multipart/form-data" method="post">
+        <input name="username" type="text">
+        <input name="password" type="password">
         <input name="file" type="file">
         <input type="submit">
     </form>
     """
     
-@app.post("/upload_form")
-def upload_form(file: UploadFile = File(...)):
+@app.post("/upload")
+def upload_form(file: UploadFile = File(...), username: str = Depends(get_current_username)):
     name = file.filename
     f = file.file
     res = images.put(name, f)
-    return res
+    return {"img_name": res,
+            "uploaded_by": username
+    }
 
-@app.get("/image_form/{name}")
+@app.get("/image/{name}")
 def download_img(name: str):
     res = images.get(name)
     return StreamingResponse(res.iter_chunks(1024), media_type="image/png")
-    
-"""
-@app.delete("/delete/{imgID}")
-@limiter.limit("15/minute")
-async def delete(imgID, request: Request, username: str = Depends(get_current_username)):
-    try:
-        with open(directory+"/image_data/"+imgID+".json", "r") as imgd:
-            imgdata = imgd.read()
-            imgdata = json.loads(imgdata)
-        os.remove(directory+"/images/"+imgID+imgdata["file_type"])
-        os.remove(directory+"/image_data/"+imgID+".json")
-        raise HTTPException(status_code=200, detail="Image deleted successfully")
-    except IOError:
-        raise HTTPException(status_code=404, detail="File not found")
-    
-@app.get("/shields/{shield_type}")
-@limiter.limit("100/minute")
-def shields(shield_type: str, request: Request):
-    if shield_type == "total": 
-        DIR = directory+"/images"
-        img_count = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
-        return {
-            "schemaVersion": 1,
-            "label": "total",
-            "message": str(img_count) + " images",
-            "color": "blue"
-            }
-    raise HTTPException(status_code=404, detail="Type not found")
-"""
 
 
 if __name__ == "__main__":
